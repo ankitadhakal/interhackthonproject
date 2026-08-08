f62883e22141d720ae5a07a50116877050b13092
Aarosh Niraula <115689963+sushant5101@users.noreply.github.com>
2026-08-09 00:20:39 +0545

    added pin image and other map logics

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,3 +6,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+function onMapClick(e) {
+    var marker = L.marker(e.latlng,{icon: greenMarker}).addTo(map);
+    marker.bindPopup("hello");
+}
+
+var greenMarker = L.icon({
+    iconUrl: "/img/Gpin.png",
+    iconSize: [38, 40],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+});
+var redMarker = L.icon({
+    iconUrl: "/img/Rpin.png",
+    iconSize: [38, 40],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+});
+
+map.on('click', onMapClick);
